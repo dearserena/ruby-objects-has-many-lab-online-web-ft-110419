a57e146e781b01038211674248f94d9dda97057a
serena Lee <dearserenalee@gmail.com>
2019-12-04 20:01:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,18 @@
-class Song
-  attr_accessor :name, :artist
-  
-  @@all = []
+class Song	
+  attr_accessor :name, :artist	
 
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
+  @@all = []	
 
-  def artist_name
-    if @artist == nil
-      return nil
-    else
-      @artist.name
-    end
-  end
-  def self.all
-    @@all
-  end
+  def initialize(name)	
+    @name = name	
+    @@all << self	
+  end	
+
+  def self.all	
+    @@all	
+  end	
+
+  def artist_name	
+    artist.name if artist	
+  end	
 end
