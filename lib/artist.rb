0cc65bcc523f636c9c46e5637c12fb21769786a9
serena Lee <dearserenalee@gmail.com>
2019-12-04 19:36:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,13 +1,12 @@
 class Artist
-  attr_accessor :name
+
+  attr_accessor :name, :songs
+
   @@song_count = 0
+
   def initialize(name)
     @name = name
-    @songs = [] 
-  end
-
-  def songs
-    @songs
+    @songs = []
   end
 
   def add_song(song)
@@ -16,8 +15,8 @@ class Artist
     @@song_count += 1
   end
 
-  def add_song_by_name(song_name)
-    song = Song.new(song_name)
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
     @@song_count += 1
@@ -27,4 +26,4 @@ class Artist
     @@song_count
   end
 
-end 
+end
