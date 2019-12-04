746d585db234c564e818811e53241698a6dc28c1
serena Lee <dearserenalee@gmail.com>
2019-12-04 20:01:28 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,17 +1,18 @@
-class Post
+class Post	
+  attr_accessor :title, :author	
 
-  attr_accessor :title, :author
-  
-  
-  def initialize(title)
-    @title = title
-  end
+  @@all = []	
 
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
-  end
+  def initialize(title)	
+    @title = title	
+    @@all << self	
+  end	
+
+  def self.all	
+    @@all	
+  end	
+
+  def author_name	
+    author.name if author	
+  end	
 end
